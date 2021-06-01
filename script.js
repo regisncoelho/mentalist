aa055e695b8aa2a4e392702f3ccb6f5f1995e4af
Regis Coelho <regis.andre09@gmail.com>
2021-06-01 02:01:30 -0300

    Changes the way tips are showed

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function restart01 () {
     document.querySelector(".game-fieldset").classList.toggle("invisible")
     document.querySelector(".result02").classList.toggle("visible")
     document.querySelector(".button-restart01").classList.toggle("visible")
+    document.querySelector(".text-tip").innerText = ""
     numeroSecreto = parseInt(Math.random() * 10)
     tentativas = 0
 }
@@ -16,13 +17,14 @@ function restart02 () {
     document.querySelector(".game-fieldset").classList.toggle("invisible")
     document.querySelector(".result01").classList.toggle("visible")
     document.querySelector(".button-restart02").classList.toggle("visible")
+    document.querySelector(".text-tip").innerText = ""
     numeroSecreto = parseInt(Math.random() * 10)
     tentativas = 0
 }
 
 function game() {
+        document.querySelector(".text-tip").innerText = ""
         if (numeroSecreto == Number(document.getElementById("resposta").value)) {
-        alert("Acertou")
         const inputField = document.querySelector(".guess-input")
         inputField.value = ""
         document.querySelector(".game-fieldset").classList.toggle("invisible")
@@ -30,20 +32,22 @@ function game() {
         document.querySelector(".button-restart01").classList.toggle("visible")
 
         } else if (Number(document.getElementById("resposta").value) > numeroSecreto) {
-        alert("O número secreto é menor")
+        document.querySelector(".text-tip").innerText = "O número secreto é menor"
         const inputField = document.querySelector(".guess-input")
         inputField.value = ""
         tentativas++
 
         } else if (Number(document.getElementById("resposta").value) < numeroSecreto) {
-        alert("O número secreto é maior")
+        
+        document.querySelector(".text-tip").innerText = "O número secreto é maior"
         const inputField = document.querySelector(".guess-input")
         inputField.value = ""
         tentativas++
         }
 
         if (tentativas >= 3) {
-        alert("Suas tentativas acabaram. O número secreto era " + numeroSecreto)
+        document.getElementById("text-result01").innerText = "As tentativas acabaram."
+        document.getElementById("text-result02").innerText = "O número secreto era " + numeroSecreto
         document.querySelector(".game-fieldset").classList.toggle("invisible")
         document.querySelector(".result01").classList.toggle("visible")
         document.querySelector(".button-restart02").classList.toggle("visible")
