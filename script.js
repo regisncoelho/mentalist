e1399ca551532c004b756b76e6738bbaeaca44cb
Regis Coelho <regis.andre09@gmail.com>
2021-06-01 01:25:02 -0300

    creates two new functions to restart the game. The function game gained new actions in order to hide and reveal components

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 var tentativas = 0
 var numeroSecreto = parseInt(Math.random() * 10)
 document.getElementById("button-answer").addEventListener("click",game)
+document.getElementById("button-restart01").addEventListener("click",restart01)
+document.getElementById("button-restart02").addEventListener("click",restart02)
+
+function restart01 () {
+    document.querySelector(".game-fieldset").classList.toggle("invisible")
+    document.querySelector(".result02").classList.toggle("visible")
+    document.querySelector(".button-restart01").classList.toggle("visible")
+    numeroSecreto = parseInt(Math.random() * 10)
+    tentativas = 0
+}
+
+function restart02 () {
+    document.querySelector(".game-fieldset").classList.toggle("invisible")
+    document.querySelector(".result01").classList.toggle("visible")
+    document.querySelector(".button-restart02").classList.toggle("visible")
+    numeroSecreto = parseInt(Math.random() * 10)
+    tentativas = 0
+}
 
 function game() {
         if (numeroSecreto == Number(document.getElementById("resposta").value)) {
         alert("Acertou")
         const inputField = document.querySelector(".guess-input")
         inputField.value = ""
-        document.querySelector(".game-fieldset").style.display = "none"
-        document.querySelector(".result02").style.display = "flex"
+        document.querySelector(".game-fieldset").classList.toggle("invisible")
+        document.querySelector(".result02").classList.toggle("visible")
+        document.querySelector(".button-restart01").classList.toggle("visible")
 
         } else if (Number(document.getElementById("resposta").value) > numeroSecreto) {
         alert("O número secreto é menor")
@@ -25,10 +44,8 @@ function game() {
 
         if (tentativas >= 3) {
         alert("Suas tentativas acabaram. O número secreto era " + numeroSecreto)
-        document.querySelector(".game-fieldset").style.display = "none"
-        document.querySelector(".result01").style.display = "flex"
+        document.querySelector(".game-fieldset").classList.toggle("invisible")
+        document.querySelector(".result01").classList.toggle("visible")
+        document.querySelector(".button-restart02").classList.toggle("visible")
         }
 }
-
-console.log(numeroSecreto)
-
